d1c50e698c998cac7c7dbeb2ad13cf52427190a6
erase2004 <erase2004@gmail.com>
2022-03-30 09:45:52 +0800

    Fix image size issue

--- src/js/imageListData/201802.js
+++ src/js/imageListData/201802.js
@@ -3,15 +3,15 @@ const baseUrl = 'https://r3-museum.acgn-stock.com/company/detail'
 module.exports = [
   {
     name: '07.png',
-    width: 1315,
-    height: 1244,
+    width: 1456,
+    height: 900,
     color: '#c4b8b5',
     url: `${baseUrl}/DfoyQ3mvGqb4yarpk`
   },
   {
     name: '08.jpeg',
-    width: 1456,
-    height: 900,
+    width: 596,
+    height: 1000,
     color: '#ad9e95',
     url: `${baseUrl}/i3yTEttyKgM7eJohz`
   },
