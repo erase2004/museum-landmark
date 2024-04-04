6e2e9d1cac207eb4552b176e9cff5be9ca7877da
Tsuki Akiba <erase2004@gmail.com>
2024-04-04 19:32:54 +0800

    refactor: update imagemin config

--- image-to-webp.mjs
+++ image-to-webp.mjs
@@ -2,19 +2,20 @@ import imagemin from 'imagemin'
 import imageminWebp from 'imagemin-webp'
 import imageminGif2webp from 'imagemin-gif2webp'
 
-imagemin(['src/assets/images/*.{jpg,jpeg,png}'], {
+imagemin(['src/assets/new-images/*.{jpg,jpeg,png}'], {
   destination: 'src/assets/images',
   plugins: [
     imageminWebp({ quality: 60 })
   ]
 })
 
-imagemin(['src/assets/images/*.gif'], {
+imagemin(['src/assets/new-images/*.gif'], {
   destination: 'src/assets/images',
   plugins: [
     imageminGif2webp({
       lossy: true,
-      quality: 50
+      quality: 50,
+      mumultiThreading: true
     })
   ]
 })
